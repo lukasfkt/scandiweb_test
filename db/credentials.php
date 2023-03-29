010e9086e6b72fd269b998d196120c8c412331ac
Tutty <tuttytanaka@hotmail.com>
2023-03-29 16:48:41 -0300

    Fixing db credentials

--- db/credentials.php
+++ db/credentials.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DB_HOST", "localhost");
+define("DB_HOST", "scandiwebassignment.ckr2lqevswdp.sa-east-1.rds.amazonaws.com:3306");
 define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_PASS", "scandiweb123");
 define("DB_NAME", "scandiweb_assignment");
