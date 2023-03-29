c8d008068c63c69def58e57cb4667c9e149c1986
Tutty <tuttytanaka@hotmail.com>
2023-03-29 18:35:47 -0300

    QA TEST PLEASEEEE

--- public/js/list.js
+++ public/js/list.js
@@ -16,8 +16,35 @@ $(document).ready(() => {
     $.post("/deleteproducts", payload, function (data, status) {})
       .done(function () {
         $("input:checkbox[name=deleteProduct]:checked").each(function () {
-          $(this).parent().hide();
+          $(this).parent().remove();
         });
+        location.reload();
+      })
+      .fail(function () {
+        $(".error-message").html("Error");
+      });
+  });
+
+  $("#product_list").submit(function (e) {
+    e.preventDefault();
+    var productsId = [];
+
+    $("input:checkbox[name=deleteProduct]:checked").each(function () {
+      productsId.push($(this).val());
+    });
+
+    if (productsId.length === 0) {
+      $(".error-message").html("Please select at least one item to delete");
+      return;
+    }
+
+    const payload = JSON.stringify(productsId);
+    $.post("/deleteproducts", payload, function (data, status) {})
+      .done(function () {
+        $("input:checkbox[name=deleteProduct]:checked").each(function () {
+          $(this).parent().remove();
+        });
+        location.reload();
       })
       .fail(function () {
         $(".error-message").html("Error");
